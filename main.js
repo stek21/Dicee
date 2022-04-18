a7cd09e5397dce7b36eff8b89fb9e4c4027d881a
Ekaterina Starovoitova <96992117+stek21@users.noreply.github.com>
2022-04-18 14:09:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var randomNumber1 = Math.floor(Math.random()*6) +1;
 var randomNumber2 = Math.floor(Math.random()*6) +1;
 
-var randomImage1 = "images/dice" + randomNumber1 + ".png";
-var randomImage2 = "images/dice" + randomNumber2 + ".png";
+var randomImage1 = "dice" + randomNumber1 + ".png";
+var randomImage2 = "dice" + randomNumber2 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 var image2 = document.querySelectorAll("img")[1];
